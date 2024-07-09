917153dd5c85eac2758a79dd985642a3dcff8757
Meenal-Sekar <123550895+Meenal-Sekar@users.noreply.github.com>
2024-07-09 12:05:28 +0530

    Add files via upload

---
+++ Signupmodel.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose');
+
+const signUpTemplate=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true,
+        },
+        contact:
+        {
+            type:String,
+            required:true
+        },
+        age:{
+            type:String,
+            required:true
+        },
+        jobRole:{
+            type:String,
+            required:true
+        },
+        date:{
+            type:Date,
+            default:Date.now
+        }
+    })
+    
+
+
+
+    module.exports=mongoose.model('sampletable',signUpTemplate)
