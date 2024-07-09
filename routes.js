97e3e187dbb58c1ec4bf6019746366f12dfc04b0
Meenal-Sekar <123550895+Meenal-Sekar@users.noreply.github.com>
2024-07-09 12:05:57 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const router=express.Router();
+
+//database schema
+
+const SignupTemplatecopy=require('../model/Signupmodel');//database schemea;
+
+router.post("/signup",(request,response)=>
+    {
+        const sam=new SignupTemplatecopy({
+    
+            name:request.body.name, //fullname(front end form name=fullName)
+            email:request.body.email,
+            contact:request.body.contact,
+            age:request.body.age,
+            jobRole:request.body.jobRole
+        })
+    
+    
+        sam.save().then(data=>
+            {
+                response.json(data);
+                console.log('Data added successfully');
+            }).catch(error=>
+                {
+    
+                    response.json(error)
+                })
+    })
+    
+    
+    module.exports=router;
+    
+    
