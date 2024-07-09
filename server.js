1228dcff4628c32d82690c3436de1b5a92eafa34
Meenal-Sekar <123550895+Meenal-Sekar@users.noreply.github.com>
2024-07-09 12:04:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const app=express();
+const mongoose = require('mongoose');
+const routerurl=require('./routes/routes')  //router-js
+const cors=require('cors'); //supporting middleware package
+
+app.use(express.json());//middleware
+app.use(cors())
+
+app.use("/app",routerurl);
+
+if(mongoose.connect('mongodb+srv://meenal-sekar:1805vishu@cluster0.nlmbzwb.mongodb.net/Final-db?retryWrites=true&w=majority&appName=Cluster0'))
+    {
+        console.log("Database connected");
+    }
+
+app.listen(4005,()=>
+{
+    console.log('server is up and run');
+})
+
+
+
+
+
+
+// db create/table structure  --->routers---->server-js
